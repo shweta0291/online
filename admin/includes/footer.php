afb8e21ff779b871467710cdbd11274aace3c975
shweta das <shwetadas@shwetas-MacBook-Air.local>
2020-12-05 11:36:29 +0000

    first commit

---
+++ admin/includes/footer.php
@@ -0,0 +1,11 @@
+ <div class="footer-copyright-area">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="footer-copy-right">
+                            <p>Online Furniture Store Mahament System &copy; 2020.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
